de041e109d23c57e855df118eb51ed4435cd7e41
Dylan <mail@dylanryan.de>
2025-02-25 00:38:29 +0100

    Fix GitHub sign-in issue with null $lastName
    Github doesn't provide a last name.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/laravel/workos?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/User.php
+++ src/User.php
@@ -7,7 +7,7 @@ class User
     public function __construct(
         public string $id,
         public string $firstName,
-        public string $lastName,
+        public ?string $lastName,
         public string $email,
         public ?string $avatar = null,
     ) {}

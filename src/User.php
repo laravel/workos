679799daab8da1d9eb88dc70f250a1ea4c4db5b7
Dylan Ryan <mail@dylanryan.de>
2025-02-25 01:13:20 +0100

    Also make firstName nullable

--- src/User.php
+++ src/User.php
@@ -6,7 +6,7 @@ class User
 {
     public function __construct(
         public string $id,
-        public string $firstName,
+        public ?string $firstName,
         public ?string $lastName,
         public string $email,
         public ?string $avatar = null,

b8fa0bf0b8a7ccabc4a6e2a7b760fb92d22dbaa1
Taylor Otwell <taylor@laravel.com>
2025-02-27 11:41:11 -0700

    Update User.php

--- src/User.php
+++ src/User.php
@@ -6,10 +6,10 @@ class User
 {
     public function __construct(
         public string $id,
+        public ?string $organizationId,
         public ?string $firstName,
         public ?string $lastName,
         public string $email,
-        public ?string $organizationId,
         public ?string $avatar = null,
     ) {}
 }
